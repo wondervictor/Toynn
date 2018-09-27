f91111a03d4f062f302a8a4119f1f46559d98302
wondervictor <victorchanchina@gmail.com>
2018-09-28 07:56:50 +0800

    add acitivtion

--- activation.py
+++ activation.py
@@ -5,18 +5,108 @@
 import numpy as np
 
 
-class FullyConnected:
+class Sigmoid:
 
-    act = None
-    weights = None
-    bias = None
+    cache = None
 
-    def __init__(self,):
+    def __init__(self):
         pass
 
-    def forward(self, x):
-        pass
+    def __call__(self, x):
+        self.cache = x
+        # TODO: Sigmoid Computation
+        return x
 
     def backward(self, grad_in):
+        x = self.cache
+        # TODO: Gradient Computation
+        return x
+
+
+class ReLU:
+
+    cache = None
+
+    def __init__(self):
         pass
 
+    def __call__(self, x):
+        self.cache = x
+        # TODO: Sigmoid Computation
+        return x
+
+    def backward(self, grad_in):
+        x = self.cache
+        # TODO: Gradient Computation
+        return x
+
+
+class LeakyReLU:
+    cache = None
+    negative_slope = 1e-2
+
+    def __init__(self, negative_slope):
+        self.negative_slope = negative_slope
+
+    def __call__(self, x):
+        self.cache = x
+        # TODO: Sigmoid Computation
+        return x
+
+    def backward(self, grad_in):
+        x = self.cache
+        # TODO: Gradient Computation
+        return x
+
+
+class Softmax:
+
+    cache = None
+
+    def __init__(self):
+        pass
+
+    def __call__(self, x):
+        self.cache = x
+        # TODO: Sigmoid Computation
+        return x
+
+    def backward(self, grad_in):
+        x = self.cache
+        # TODO: Gradient Computation
+        return x
+
+
+class Tanh:
+    cache = None
+
+    def __init__(self):
+        pass
+
+    def __call__(self, x):
+        self.cache = x
+        # TODO: Sigmoid Computation
+        return x
+
+    def backward(self, grad_in):
+        x = self.cache
+        # TODO: Gradient Computation
+        return x
+
+
+class ELU:
+
+    cache = None
+
+    def __init__(self):
+        pass
+
+    def __call__(self, x):
+        self.cache = x
+        # TODO: Sigmoid Computation
+        return x
+
+    def backward(self, grad_in):
+        x = self.cache
+        # TODO: Gradient Computation
+        return x
